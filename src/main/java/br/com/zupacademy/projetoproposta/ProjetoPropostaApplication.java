1cefda3dec572997163fa7262f560371dd8e4747
William Bohn <williamxavero@gmail.com>
2021-06-03 16:36:55 -0300

    setup

---
+++ src/main/java/br/com/zupacademy/projetoproposta/ProjetoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.projetoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/projetoproposta/ProjetoPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.projetoproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
